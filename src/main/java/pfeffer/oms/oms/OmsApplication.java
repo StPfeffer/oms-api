3b29a3556656e77dda5a38c9ba95c463ae8c1b69
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 20:06:11 -0300

    Initial commit

---
+++ src/main/java/pfeffer/oms/oms/OmsApplication.java
@@ -0,0 +1,13 @@
+package pfeffer.oms.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pfeffer/oms/oms/OmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pfeffer.oms.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
