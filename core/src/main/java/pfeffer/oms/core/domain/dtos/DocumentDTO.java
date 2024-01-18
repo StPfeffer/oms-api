ed447ff079acb8e6373c92a0bddb9f9cd0a89539
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 21:24:24 -0300

    Criação do DocumentDTO

---
+++ core/src/main/java/pfeffer/oms/core/domain/dtos/DocumentDTO.java
@@ -0,0 +1,15 @@
+package pfeffer.oms.core.domain.dtos;
+
+public final class DocumentDTO {
+
+    /**
+     * O tipo do documento informado.
+     */
+    private String type;
+
+    /**
+     * O número do documento informado.
+     */
+    private String number;
+
+}
