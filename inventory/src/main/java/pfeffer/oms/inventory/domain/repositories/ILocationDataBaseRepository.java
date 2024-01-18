c4555f0cae48d68afa87b393226f3496a53e0ece
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 22:37:06 -0300

    Criação da interface ILocationDataBaseRepository

---
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ILocationDataBaseRepository.java
@@ -0,0 +1,6 @@
+package pfeffer.oms.inventory.domain.repositories;
+
+import pfeffer.oms.inventory.domain.entities.LocationBO;
+
+public interface ILocationDataBaseRepository extends IAbstractDataBaseRepository<LocationBO> {
+}
