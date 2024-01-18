dc20bbf97cd5ebefd1674c4d08eaf7e8f3cf1d86
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 22:22:00 -0300

    Implementação dos getters & setters para o TelephoneDTO

--- core/src/main/java/pfeffer/oms/core/domain/dtos/TelephoneDTO.java
+++ core/src/main/java/pfeffer/oms/core/domain/dtos/TelephoneDTO.java
@@ -6,4 +6,28 @@ public class TelephoneDTO {
     private String ddd;
     private String number;
 
+    public String getDdi() {
+        return ddi;
+    }
+
+    public void setDdi(String ddi) {
+        this.ddi = ddi;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
 }
