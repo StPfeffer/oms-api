1ea38f57aad6984bfd2eaaf7025759f8ccaa1bbf
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 21:31:46 -0300

    Vinculo do AddressDTO na LocationDTO

--- inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/LocationDTO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/LocationDTO.java
@@ -1,5 +1,7 @@
 package pfeffer.oms.inventory.domain.dtos;
 
+import pfeffer.oms.core.domain.dtos.AddressDTO;
+
 import java.util.Date;
 
 public class LocationDTO {
@@ -30,9 +32,9 @@ public class LocationDTO {
     private String description;
 
     /**
-     * O CEP da filial.
+     * As informações de endereço da filial.
      */
-    private String zip;
+    private AddressDTO address;
 
     /**
      * A data de criação da filial.
@@ -43,69 +45,5 @@ public class LocationDTO {
      * A data da última atualização da filial.
      */
     private Date updatedAt;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTradingName() {
-        return tradingName;
-    }
-
-    public void setTradingName(String tradingName) {
-        this.tradingName = tradingName;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 
 }
