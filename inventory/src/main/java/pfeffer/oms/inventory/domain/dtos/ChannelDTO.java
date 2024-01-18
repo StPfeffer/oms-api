123c5273280897038e96ec665830834cc1b8b94e
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 21:15:56 -0300

    Implementação dos getters & setters para o ChannelDTO

--- inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/ChannelDTO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/ChannelDTO.java
@@ -12,4 +12,20 @@ public class ChannelDTO {
      */
     private String name;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
