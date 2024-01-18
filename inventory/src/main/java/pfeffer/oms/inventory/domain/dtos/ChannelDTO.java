c8019b9401c8305b8feb0618ef2d2826b59fcc3b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 21:08:18 -0300

    Criação do ChannelDTO

---
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/ChannelDTO.java
@@ -0,0 +1,15 @@
+package pfeffer.oms.inventory.domain.dtos;
+
+public class ChannelDTO {
+
+    /**
+     * O ID do canal de vendas.
+     */
+    private Integer id;
+
+    /**
+     * O nome do canal de vendas.
+     */
+    private String name;
+
+}
