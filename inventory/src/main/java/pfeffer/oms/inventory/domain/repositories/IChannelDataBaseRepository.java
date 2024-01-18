90a9fba63de44e170deb1a8d75d7d0c2ca1db3a6
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 22:36:59 -0300

    Ajuste na IChannelDataBaseRepository para extender a nova interface

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IChannelDataBaseRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IChannelDataBaseRepository.java
@@ -2,8 +2,5 @@ package pfeffer.oms.inventory.domain.repositories;
 
 import pfeffer.oms.inventory.domain.entities.ChannelBO;
 
-public interface IChannelDataBaseRepository {
-
-    ChannelBO persist(ChannelBO bo);
-
+public interface IChannelDataBaseRepository extends IAbstractDataBaseRepository<ChannelBO> {
 }
