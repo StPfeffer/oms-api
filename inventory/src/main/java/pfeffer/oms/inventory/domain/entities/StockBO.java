9a41304e4550753d58778118a3724d1d33571aee
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 22:30:12 -0300

    Remoção de javadocs

--- inventory/src/main/java/pfeffer/oms/inventory/domain/entities/StockBO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/entities/StockBO.java
@@ -5,34 +5,16 @@ import pfeffer.oms.inventory.domain.enums.EnumStockType;
 
 public class StockBO {
 
-    /**
-     * O ID do SKU no qual o estoque está vinculado.
-     */
     private String skuId;
 
-    /**
-     * O ID da filial na qual o estoque pertence.
-     */
     private LocationDTO location;
 
-    /**
-     * O saldo total para o respectivo SKU.
-     */
     private Integer quantity;
 
-    /**
-     * A proteção de estoque para o respectivo SKU.
-     */
     private Integer threshold;
 
-    /**
-     * O tipo do estoque.
-     */
     private EnumStockType stockType;
 
-    /**
-     * Determina se esse saldo está disponível para o respectivo SKU.
-     */
     private boolean enabled;
 
     public String getSkuId() {
