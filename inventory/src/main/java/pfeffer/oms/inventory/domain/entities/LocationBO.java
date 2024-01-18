6a59ab7d7f869751178f17843e1852489dc9c76e
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 22:09:42 -0300

    Criação do LocationBO

---
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/entities/LocationBO.java
@@ -0,0 +1,89 @@
+package pfeffer.oms.inventory.domain.entities;
+
+import pfeffer.oms.core.domain.dtos.AddressDTO;
+
+import java.util.Date;
+
+public class LocationBO {
+
+    private String id;
+
+    private String tradingName;
+
+    private String alias;
+
+    private String name;
+
+    private String description;
+
+    private AddressDTO address;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTradingName() {
+        return tradingName;
+    }
+
+    public void setTradingName(String tradingName) {
+        this.tradingName = tradingName;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public AddressDTO getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressDTO address) {
+        this.address = address;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
