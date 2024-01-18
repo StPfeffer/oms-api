05a0df8547c4741e7c33512de82bbbc6a12f8b08
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 22:49:16 -0300

    Removendo propriedade ID do ChannelBO

--- inventory/src/main/java/pfeffer/oms/inventory/domain/entities/ChannelBO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/entities/ChannelBO.java
@@ -1,16 +1,7 @@
 package pfeffer.oms.inventory.domain.entities;
 
 public class ChannelBO {
-    private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
