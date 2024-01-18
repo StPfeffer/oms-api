63ebc1470161bcb135c7b028647a6f848e6a350c
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 21:24:36 -0300

    Criação do AddressDTO

---
+++ core/src/main/java/pfeffer/oms/core/domain/dtos/AddressDTO.java
@@ -0,0 +1,19 @@
+package pfeffer.oms.core.domain.dtos;
+
+public class AddressDTO {
+
+    private String zip;
+
+    private String number;
+
+    private String address;
+
+    private String neighbourhood;
+
+    private String city;
+
+    private String state;
+
+    private String country;
+
+}
