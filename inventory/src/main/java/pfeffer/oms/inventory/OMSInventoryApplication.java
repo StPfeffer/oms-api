5839fd27a4768ecc2a5a5763c530a78a7b232c66
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 21:04:37 -0300

    Criação do módulo Inventory

---
+++ inventory/src/main/java/pfeffer/oms/inventory/OMSInventoryApplication.java
@@ -0,0 +1,13 @@
+package pfeffer.oms.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OMSInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OMSInventoryApplication.class, args);
+    }
+
+}
