a811883aa3b24faf1945c48004c4a6c4dcdef8bb
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 22:26:47 -0300

    Implementação dos getters & setters para o StockDTO

--- inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/StockDTO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/StockDTO.java
@@ -34,4 +34,52 @@ public class StockDTO {
      */
     private boolean enabled;
 
+    public String getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(String skuId) {
+        this.skuId = skuId;
+    }
+
+    public LocationDTO getLocation() {
+        return location;
+    }
+
+    public void setLocation(LocationDTO location) {
+        this.location = location;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(Integer threshold) {
+        this.threshold = threshold;
+    }
+
+    public EnumStockType getStockType() {
+        return stockType;
+    }
+
+    public void setStockType(EnumStockType stockType) {
+        this.stockType = stockType;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
 }
