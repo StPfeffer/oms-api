64153a79f770b82398431be8576eff97e0cef6a4
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 22:21:47 -0300

    Criação do TelephoneBO

---
+++ core/src/main/java/pfeffer/oms/core/domain/entities/TelephoneBO.java
@@ -0,0 +1,33 @@
+package pfeffer.oms.core.domain.entities;
+
+public class TelephoneBO {
+
+    private String ddi;
+    private String ddd;
+    private String number;
+
+    public String getDdi() {
+        return ddi;
+    }
+
+    public void setDdi(String ddi) {
+        this.ddi = ddi;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+}
