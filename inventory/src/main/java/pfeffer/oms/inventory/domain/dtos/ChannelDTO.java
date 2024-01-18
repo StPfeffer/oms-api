35eaad50f815dee1f2d9429e2322d7006dd8724d
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-17 22:49:11 -0300

    Removendo propriedade ID do ChannelDTO

--- inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/ChannelDTO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/ChannelDTO.java
@@ -3,22 +3,9 @@ package pfeffer.oms.inventory.domain.dtos;
 public class ChannelDTO {
 
     /**
-     * O ID do canal de vendas.
-     */
-    private Long id;
-
-    /**
      * O nome do canal de vendas.
      */
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
