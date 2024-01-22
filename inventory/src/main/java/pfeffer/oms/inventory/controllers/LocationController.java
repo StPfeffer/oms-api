9b48b476081771745f1634531f3d9464f4e9702d
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-22 18:49:56 -0300

    Implementação do LocationController

---
+++ inventory/src/main/java/pfeffer/oms/inventory/controllers/LocationController.java
@@ -0,0 +1,44 @@
+package pfeffer.oms.inventory.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pfeffer.oms.inventory.domain.dtos.LocationDTO;
+import pfeffer.oms.inventory.services.LocationService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("locations")
+public class LocationController {
+
+    private final LocationService service;
+
+    @Autowired
+    public LocationController(LocationService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<LocationDTO> createLocation(@RequestBody LocationDTO location) {
+        LocationDTO newChannel = this.service.createLocation(location);
+
+        return new ResponseEntity<>(newChannel, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LocationDTO>> listLocations() {
+        List<LocationDTO> locations = this.service.listLocations();
+
+        return new ResponseEntity<>(locations, HttpStatus.OK);
+    }
+
+    @GetMapping("{locationId}")
+    public ResponseEntity<LocationDTO> getLocation(@PathVariable String locationId) {
+        LocationDTO location = this.service.findLocationByLocationId(locationId);
+
+        return new ResponseEntity<>(location, HttpStatus.OK);
+    }
+
+}
