7dfcd3f76d70ccb71cc0cee04c8987f0882cab00
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-22 18:29:42 -0300

    Implementação do JakartaLocationMapper

---
+++ inventory/src/main/java/pfeffer/oms/inventory/jakarta/mappers/JakartaLocationMapper.java
@@ -0,0 +1,39 @@
+package pfeffer.oms.inventory.jakarta.mappers;
+
+import pfeffer.oms.core.infra.jakarta.mappers.JakartaAddressMapper;
+import pfeffer.oms.inventory.domain.entities.LocationBO;
+import pfeffer.oms.inventory.jakarta.model.JakartaLocation;
+
+public class JakartaLocationMapper {
+
+    public static JakartaLocation toEntity(LocationBO bo) {
+        JakartaLocation entity = new JakartaLocation();
+
+        entity.setLocationId(bo.getId());
+        entity.setTradingName(bo.getTradingName());
+        entity.setAlias(bo.getAlias());
+        entity.setName(bo.getName());
+        entity.setDescription(bo.getDescription());
+        entity.setAddress(JakartaAddressMapper.toEntity(bo.getAddress()));
+        entity.setCreatedAt(bo.getCreatedAt());
+        entity.setUpdatedAt(bo.getUpdatedAt());
+
+        return entity;
+    }
+
+    public static LocationBO toDomain(JakartaLocation entity) {
+        LocationBO bo = new LocationBO();
+
+        bo.setId(entity.getLocationId());
+        bo.setTradingName(entity.getTradingName());
+        bo.setAlias(entity.getAlias());
+        bo.setName(entity.getName());
+        bo.setDescription(entity.getDescription());
+        bo.setAddress(JakartaAddressMapper.toDomain(entity.getAddress()));
+        bo.setCreatedAt(entity.getCreatedAt());
+        bo.setUpdatedAt(entity.getUpdatedAt());
+
+        return bo;
+    }
+
+}
