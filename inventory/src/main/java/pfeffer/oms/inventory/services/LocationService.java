f69331e8ebccc846070e71671eaf5ce0e80b1710
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-22 18:47:32 -0300

    Implementação do LocationService

---
+++ inventory/src/main/java/pfeffer/oms/inventory/services/LocationService.java
@@ -0,0 +1,49 @@
+package pfeffer.oms.inventory.services;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pfeffer.oms.inventory.domain.dtos.LocationDTO;
+import pfeffer.oms.inventory.domain.mappers.LocationMapper;
+import pfeffer.oms.inventory.domain.repositories.ILocationRepository;
+import pfeffer.oms.inventory.domain.usecases.CreateLocation;
+import pfeffer.oms.inventory.jakarta.mappers.JakartaLocationMapper;
+import pfeffer.oms.inventory.jakarta.repository.JakartaLocationRepository;
+
+import java.util.List;
+
+@Service
+public class LocationService implements ILocationRepository {
+
+    private final JakartaLocationRepository repository;
+
+    @Autowired
+    public LocationService(JakartaLocationRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public LocationDTO createLocation(LocationDTO dto) {
+        CreateLocation createLocation = new CreateLocation(repository);
+
+        return createLocation.execute(dto);
+    }
+
+    public List<LocationDTO> listLocations() {
+        return this.repository.findAll().stream().map(jakartaLocation ->
+                LocationMapper.toDTO(JakartaLocationMapper.toDomain(jakartaLocation))
+        ).toList();
+    }
+
+    @Override
+    public LocationDTO findLocationByLocationId(String locationId) {
+        LocationDTO location = this.repository.findLocationByLocationId(locationId);
+
+        if (location == null) {
+            throw new RuntimeException("Não existe um canal para o ID informado");
+        }
+
+        return location;
+    }
+
+}
