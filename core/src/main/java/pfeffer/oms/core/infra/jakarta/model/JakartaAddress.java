1a611fbf09a6d218b1695d1f07cd9d2f3c456869
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-22 18:29:08 -0300

    Criação da entidade JakartaAddress

---
+++ core/src/main/java/pfeffer/oms/core/infra/jakarta/model/JakartaAddress.java
@@ -0,0 +1,53 @@
+package pfeffer.oms.core.infra.jakarta.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pfeffer.oms.core.domain.enums.EnumState;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+@Entity(name = "address")
+public class JakartaAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "zip", nullable = false)
+    private String zip;
+
+    @Column(name = "number")
+    private String number;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "address2")
+    private String address2;
+
+    @Column(name = "neighbourhood")
+    private String neighbourhood;
+
+    @Column(name = "city")
+    private String city;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state")
+    private EnumState state;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "latitude")
+    private Integer latitude;
+
+    @Column(name = "longitude")
+    private Integer longitude;
+
+}
