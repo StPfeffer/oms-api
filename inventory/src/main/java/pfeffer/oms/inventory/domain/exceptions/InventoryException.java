b47b50a27bfbd73b2ff77d06136d67e65d6299a8
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-23 20:39:18 -0300

    Criação da InventoryException

---
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/exceptions/InventoryException.java
@@ -0,0 +1,30 @@
+package pfeffer.oms.inventory.domain.exceptions;
+
+public class InventoryException extends RuntimeException {
+    private String message;
+    private Integer statusCode;
+
+    public InventoryException(String message, Integer statusCode) {
+        super(message);
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+}
