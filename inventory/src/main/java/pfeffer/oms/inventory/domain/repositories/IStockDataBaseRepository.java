538d3e4bc90ce145c1a31c1bac906f796ff8231f
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-23 19:15:48 -0300

    Criação da interface IStockDataBaseRepository

---
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockDataBaseRepository.java
@@ -0,0 +1,6 @@
+package pfeffer.oms.inventory.domain.repositories;
+
+import pfeffer.oms.inventory.domain.entities.StockBO;
+
+public interface IStockDataBaseRepository extends IAbstractDataBaseRepository<StockBO> {
+}
