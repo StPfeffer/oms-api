7d7728840a18363fccef303e6a2d7d656eacda0b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-23 20:39:53 -0300

    Implementação da ControllerExceptionHandler

---
+++ inventory/src/main/java/pfeffer/oms/inventory/infra/ControllerExceptionHandler.java
@@ -0,0 +1,38 @@
+package pfeffer.oms.inventory.infra;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pfeffer.oms.inventory.domain.dtos.ExceptionDTO;
+import pfeffer.oms.inventory.domain.exceptions.InventoryException;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDTO> threatGeneralExceptions(Exception exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionDTO> DataIntegrityViolationExceptions(DataIntegrityViolationException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "400");
+
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(InventoryException.class)
+    public ResponseEntity<ExceptionDTO> threatChannelException(InventoryException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), String.valueOf(exception.getStatusCode()));
+
+        return switch (exception.getStatusCode()) {
+            case 400 -> ResponseEntity.notFound().build();
+            case 404 -> ResponseEntity.badRequest().body(exceptionDTO);
+            default -> ResponseEntity.internalServerError().body(exceptionDTO);
+        };
+    }
+
+}
