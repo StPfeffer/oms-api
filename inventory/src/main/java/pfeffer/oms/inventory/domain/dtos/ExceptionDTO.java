8f98e27eaf7aca2ff7960bae40b4e8b4dbc9466b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-23 20:39:58 -0300

    Implementação da ExceptionDTO

---
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/ExceptionDTO.java
@@ -0,0 +1,28 @@
+package pfeffer.oms.inventory.domain.dtos;
+
+public final class ExceptionDTO {
+    private String message;
+    private String statusCode;
+
+    public ExceptionDTO(String message, String statusCode) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+}
