5be349f9d5a872500cb76d37f84ef437debc959a
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-23 19:25:44 -0300

    Implementação do JakartaStockRepository e método persist

---
+++ inventory/src/main/java/pfeffer/oms/inventory/jakarta/repository/JakartaStockRepository.java
@@ -0,0 +1,33 @@
+package pfeffer.oms.inventory.jakarta.repository;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.stereotype.Service;
+import pfeffer.oms.inventory.domain.entities.StockBO;
+import pfeffer.oms.inventory.domain.repositories.IStockDataBaseRepository;
+import pfeffer.oms.inventory.domain.repositories.IStockRepository;
+import pfeffer.oms.inventory.jakarta.mappers.JakartaStockMapper;
+import pfeffer.oms.inventory.jakarta.model.JakartaStock;
+
+@Service
+public class JakartaStockRepository extends SimpleJpaRepository<JakartaStock, Long> implements IStockDataBaseRepository, IStockRepository {
+
+    private final EntityManager em;
+
+    public JakartaStockRepository(EntityManager em) {
+        super(JakartaStock.class, em);
+        this.em = em;
+
+    }
+
+    @Override
+    public StockBO persist(StockBO bo) {
+        JakartaStock entity = JakartaStockMapper.toEntity(bo);
+
+        em.persist(entity);
+        em.flush();
+
+        return JakartaStockMapper.toDomain(entity);
+    }
+
+}
