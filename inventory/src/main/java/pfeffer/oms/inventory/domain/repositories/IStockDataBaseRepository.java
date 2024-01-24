dc4492852c97e6427cb17a876ecc185b3900b481
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-24 20:40:59 -0300

    Criando contrato para atualizar estoque

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockDataBaseRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockDataBaseRepository.java
@@ -3,4 +3,7 @@ package pfeffer.oms.inventory.domain.repositories;
 import pfeffer.oms.inventory.domain.entities.StockBO;
 
 public interface IStockDataBaseRepository extends IAbstractDataBaseRepository<StockBO> {
+
+    StockBO update(String skuId, StockBO bo);
+
 }
