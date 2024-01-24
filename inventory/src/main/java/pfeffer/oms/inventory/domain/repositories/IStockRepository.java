6110661cbc360d7b89e3c8fba813dd35148b379b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-24 20:41:07 -0300

    Criando contrato para buscar estoque

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockRepository.java
@@ -1,4 +1,9 @@
 package pfeffer.oms.inventory.domain.repositories;
 
+import pfeffer.oms.inventory.domain.dtos.StockDTO;
+
 public interface IStockRepository {
+
+    StockDTO findStockBySkuId(String skuId);
+
 }
