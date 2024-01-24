c33fa79f4b9f547a763db163dac6b4dacc687615
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-24 20:42:05 -0300

    Implementação dos métodos para atualizar e buscar estoque

--- inventory/src/main/java/pfeffer/oms/inventory/services/StockService.java
+++ inventory/src/main/java/pfeffer/oms/inventory/services/StockService.java
@@ -4,8 +4,10 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pfeffer.oms.inventory.domain.dtos.StockDTO;
+import pfeffer.oms.inventory.domain.exceptions.StockException;
 import pfeffer.oms.inventory.domain.repositories.IStockRepository;
 import pfeffer.oms.inventory.domain.usecases.CreateStock;
+import pfeffer.oms.inventory.domain.usecases.UpdateStock;
 import pfeffer.oms.inventory.infra.jakarta.repository.JakartaStockRepository;
 
 @Service
@@ -23,6 +25,24 @@ public class StockService implements IStockRepository {
         CreateStock createStock = new CreateStock(repository);
 
         return createStock.execute(dto);
+    }
+
+    @Transactional
+    public StockDTO updateStock(String skuId, StockDTO dto) {
+        UpdateStock updateStock = new UpdateStock(repository);
+
+        return updateStock.execute(skuId, dto);
+    }
+
+    @Override
+    public StockDTO findStockBySkuId(String skuId) {
+        StockDTO stock = this.repository.findStockBySkuId(skuId);
+
+        if (stock == null) {
+            throw new StockException("Não existe um estoque para o SKU informado", 404);
+        }
+
+        return stock;
     }
 
 }
