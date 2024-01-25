3ce72f7ab43ee86af586facd4e6098982adbd620
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 20:58:18 -0300

    Remoção de contrato para update na ILocationDataBaseRepository

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ILocationDataBaseRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ILocationDataBaseRepository.java
@@ -3,7 +3,4 @@ package pfeffer.oms.inventory.domain.repositories;
 import pfeffer.oms.inventory.domain.entities.LocationBO;
 
 public interface ILocationDataBaseRepository extends IAbstractDataBaseRepository<LocationBO> {
-
-    LocationBO update(String locationId, LocationBO bo);
-
 }
