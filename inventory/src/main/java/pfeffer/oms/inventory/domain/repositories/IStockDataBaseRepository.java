0968f487d76655c7bebed92d4ac152e4e463d35c
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 20:58:22 -0300

    Remoção de contrato para update na IStockDataBaseRepository

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockDataBaseRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockDataBaseRepository.java
@@ -3,7 +3,4 @@ package pfeffer.oms.inventory.domain.repositories;
 import pfeffer.oms.inventory.domain.entities.StockBO;
 
 public interface IStockDataBaseRepository extends IAbstractDataBaseRepository<StockBO> {
-
-    StockBO update(String skuId, StockBO bo);
-
 }
