f9a07e47730847fca79d0cbe647aafb58566b4e7
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 21:22:49 -0300

    Ajuste no import do IAbstractDataBaseRepository

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockDataBaseRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockDataBaseRepository.java
@@ -1,9 +1,7 @@
 package pfeffer.oms.inventory.domain.repositories;
 
+import pfeffer.oms.core.domain.repositories.IAbstractDataBaseRepository;
 import pfeffer.oms.inventory.domain.entities.StockBO;
 
 public interface IStockDataBaseRepository extends IAbstractDataBaseRepository<StockBO> {
-
-    StockBO update(String skuId, StockBO bo);
-
 }
