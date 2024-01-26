a774ee516cd43886a471550ffff5155087253378
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 21:24:44 -0300

    Criação da interface IAddressDataBaseRepository

---
+++ core/src/main/java/pfeffer/oms/core/domain/repositories/IAddressDataBaseRepository.java
@@ -0,0 +1,6 @@
+package pfeffer.oms.core.domain.repositories;
+
+import pfeffer.oms.core.domain.entities.AddressBO;
+
+public interface IAddressDataBaseRepository extends IAbstractDataBaseRepository<AddressBO> {
+}
