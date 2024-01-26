2ef6798c1a4e7896a73cf4d8a55ecebdaab26a16
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 21:11:59 -0300

    Implementação dos getters e setters para DocumentBO

--- core/src/main/java/pfeffer/oms/core/domain/entities/DocumentBO.java
+++ core/src/main/java/pfeffer/oms/core/domain/entities/DocumentBO.java
@@ -6,4 +6,20 @@ public class DocumentBO {
 
     private String number;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
 }
