a69e6397b5bf36612b1f05c898f8e5d64f348f8f
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 21:23:56 -0300

    Removendo contrato update do ILocationDataBaseRepository
    O contrato já foi definido em IAbstractDataBaseRepository

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ILocationDataBaseRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ILocationDataBaseRepository.java
@@ -4,7 +4,4 @@ import pfeffer.oms.core.domain.repositories.IAbstractDataBaseRepository;
 import pfeffer.oms.inventory.domain.entities.LocationBO;
 
 public interface ILocationDataBaseRepository extends IAbstractDataBaseRepository<LocationBO> {
-
-    LocationBO update(String locationId, LocationBO bo);
-
 }
