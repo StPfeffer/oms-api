3cdcd6524573f81a1561f1d54212965837e88a49
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 21:24:48 -0300

    Criação da interface ICustomerDataBaseRepository

---
+++ core/src/main/java/pfeffer/oms/core/domain/repositories/ICustomerDataBaseRepository.java
@@ -0,0 +1,6 @@
+package pfeffer.oms.core.domain.repositories;
+
+import pfeffer.oms.core.domain.entities.CustomerBO;
+
+public interface ICustomerDataBaseRepository extends IAbstractDataBaseRepository<CustomerBO> {
+}
