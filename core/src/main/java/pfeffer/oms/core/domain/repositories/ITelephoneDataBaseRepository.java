36c2a092c52b6c9195fcd9ba3a39a86384eb0f47
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 21:24:55 -0300

    Criação da interface ITelephoneDataBaseRepository

---
+++ core/src/main/java/pfeffer/oms/core/domain/repositories/ITelephoneDataBaseRepository.java
@@ -0,0 +1,6 @@
+package pfeffer.oms.core.domain.repositories;
+
+import pfeffer.oms.core.domain.entities.TelephoneBO;
+
+public interface ITelephoneDataBaseRepository extends IAbstractDataBaseRepository<TelephoneBO> {
+}
