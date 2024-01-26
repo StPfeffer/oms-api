547d08360b526a79f83dc34e70463617d80f1013
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 21:22:39 -0300

    Ajuste no import do IAbstractDataBaseRepository

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IChannelDataBaseRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IChannelDataBaseRepository.java
@@ -1,5 +1,6 @@
 package pfeffer.oms.inventory.domain.repositories;
 
+import pfeffer.oms.core.domain.repositories.IAbstractDataBaseRepository;
 import pfeffer.oms.inventory.domain.entities.ChannelBO;
 
 public interface IChannelDataBaseRepository extends IAbstractDataBaseRepository<ChannelBO> {
