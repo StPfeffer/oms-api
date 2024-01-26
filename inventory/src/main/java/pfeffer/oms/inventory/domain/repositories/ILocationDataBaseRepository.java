6272c6826d7baad2ef36a156fb8409b6688ef5ae
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 21:22:36 -0300

    Ajuste no import do IAbstractDataBaseRepository

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ILocationDataBaseRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ILocationDataBaseRepository.java
@@ -1,5 +1,6 @@
 package pfeffer.oms.inventory.domain.repositories;
 
+import pfeffer.oms.core.domain.repositories.IAbstractDataBaseRepository;
 import pfeffer.oms.inventory.domain.entities.LocationBO;
 
 public interface ILocationDataBaseRepository extends IAbstractDataBaseRepository<LocationBO> {
