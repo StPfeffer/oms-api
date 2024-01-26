fc995fae77f4440e0989b951ba49a0098fe891c6
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-25 21:39:24 -0300

    Adicionada propriedade type ao TelephoneBO

--- core/src/main/java/pfeffer/oms/core/domain/entities/TelephoneBO.java
+++ core/src/main/java/pfeffer/oms/core/domain/entities/TelephoneBO.java
@@ -5,6 +5,7 @@ public class TelephoneBO {
     private String ddi;
     private String ddd;
     private String number;
+    private String type;
 
     public String getDdi() {
         return ddi;
@@ -28,6 +29,14 @@ public class TelephoneBO {
 
     public void setNumber(String number) {
         this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
 }
