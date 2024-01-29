c551590b0bf257f108ace070273bce0b76e785b6
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-29 19:56:00 -0300

    Removendo comentários

--- inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/LocationDTO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/LocationDTO.java
@@ -1,49 +1,23 @@
 package pfeffer.oms.inventory.domain.dtos;
 
-import pfeffer.oms.core.domain.dtos.AddressDTO;
-
 import java.util.Date;
 
 public class LocationDTO {
 
-    /**
-     * O ID da filial.
-     */
     private String id;
 
-    /**
-     * O nome fantasia da filial.
-     */
     private String tradingName;
 
-    /**
-     * O apelido da filial.
-     */
     private String alias;
 
-    /**
-     * A razão social da filial.
-     */
     private String name;
 
-    /**
-     * A descrição da filial.
-     */
     private String description;
 
-    /**
-     * As informações de endereço da filial.
-     */
     private AddressDTO address;
 
-    /**
-     * A data de criação da filial.
-     */
     private Date createdAt;
 
-    /**
-     * A data da última atualização da filial.
-     */
     private Date updatedAt;
 
     public String getId() {
