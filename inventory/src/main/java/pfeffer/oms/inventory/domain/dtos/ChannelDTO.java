8713579ead151d224b18a733249cec427ccbd6bf
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-30 21:43:37 -0300

    Removendo comentário

--- inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/ChannelDTO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/ChannelDTO.java
@@ -2,9 +2,6 @@ package pfeffer.oms.inventory.domain.dtos;
 
 public class ChannelDTO {
 
-    /**
-     * O nome do canal de vendas.
-     */
     private String channelId;
 
     public String getChannelId() {
