02142bab12380ad188ae32c87b49e9d79166a2f3
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-31 21:53:53 -0300

    Criando contrato update na ICustomerDataBaseRepository

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ICustomerDataBaseRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ICustomerDataBaseRepository.java
@@ -3,4 +3,7 @@ package pfeffer.oms.inventory.domain.repositories;
 import pfeffer.oms.inventory.domain.entities.CustomerBO;
 
 public interface ICustomerDataBaseRepository extends IAbstractDataBaseRepository<CustomerBO> {
+
+    CustomerBO update(String customerId, CustomerBO bo);
+
 }
