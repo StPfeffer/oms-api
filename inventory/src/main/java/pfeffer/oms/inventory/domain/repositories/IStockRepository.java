5e16319fd39f6f4ca33880085928a515d6a20afe
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-31 21:54:15 -0300

    Ajuste nos contratos da IStockRepository

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IStockRepository.java
@@ -2,8 +2,12 @@ package pfeffer.oms.inventory.domain.repositories;
 
 import pfeffer.oms.inventory.domain.dtos.StockDTO;
 
+import java.util.List;
+
 public interface IStockRepository {
 
-    StockDTO findStockBySkuId(String skuId);
+    List<StockDTO> listStockBySkuId(String skuId);
+
+    StockDTO findStockBySkuIdAndLocationId(String skuId, String locationId);
 
 }
