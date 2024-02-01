6a555bef9953603da8eb6d24b63f8b5ddb095a58
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-01-31 21:53:58 -0300

    Criando contrato update na ILocationDataBaseRepository

--- inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ILocationDataBaseRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/ILocationDataBaseRepository.java
@@ -3,4 +3,7 @@ package pfeffer.oms.inventory.domain.repositories;
 import pfeffer.oms.inventory.domain.entities.LocationBO;
 
 public interface ILocationDataBaseRepository extends IAbstractDataBaseRepository<LocationBO> {
+
+    LocationBO update(String locationId, LocationBO bo);
+
 }
