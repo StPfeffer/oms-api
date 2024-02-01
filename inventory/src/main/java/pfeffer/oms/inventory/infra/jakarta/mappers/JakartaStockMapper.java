911ab04beea6942352b1c4678151408d970e6692
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-01 19:21:22 -0300

    Ajuste no JakartaStockMapper para realizar o mapeamento da filial

--- inventory/src/main/java/pfeffer/oms/inventory/infra/jakarta/mappers/JakartaStockMapper.java
+++ inventory/src/main/java/pfeffer/oms/inventory/infra/jakarta/mappers/JakartaStockMapper.java
@@ -1,19 +1,15 @@
 package pfeffer.oms.inventory.infra.jakarta.mappers;
 
-import pfeffer.oms.inventory.domain.entities.LocationBO;
 import pfeffer.oms.inventory.domain.entities.StockBO;
 import pfeffer.oms.inventory.infra.jakarta.model.JakartaLocation;
 import pfeffer.oms.inventory.infra.jakarta.model.JakartaStock;
 
 public class JakartaStockMapper {
 
-    public static JakartaStock toEntity(StockBO bo, LocationBO locationBO) {
+    public static JakartaStock toEntity(StockBO bo, JakartaLocation jakartaLocation) {
         JakartaStock entity = new JakartaStock();
 
         entity.setSkuId(bo.getSkuId());
-
-        JakartaLocation jakartaLocation = JakartaLocationMapper.toEntity(locationBO);
-
         entity.setLocation(jakartaLocation);
         entity.setQuantity(bo.getQuantity());
         entity.setThreshold(bo.getThreshold());
