5cdc778f91e023493ccba1367f07d06cf153c1cb
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-02 23:40:15 -0300

    Implementação dos getters e setters para o LocationChannelBO

--- inventory/src/main/java/pfeffer/oms/inventory/domain/entities/LocationChannelBO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/entities/LocationChannelBO.java
@@ -8,4 +8,28 @@ public class LocationChannelBO {
 
     private boolean enabled;
 
+    public String getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(String locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(String channelId) {
+        this.channelId = channelId;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
 }
