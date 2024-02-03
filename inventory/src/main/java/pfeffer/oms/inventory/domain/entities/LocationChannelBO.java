76457da6b7b889b771966b0eea083fabbd14159a
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-02 23:38:20 -0300

    Criação do LocationChannelBO

---
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/entities/LocationChannelBO.java
@@ -0,0 +1,11 @@
+package pfeffer.oms.inventory.domain.entities;
+
+public class LocationChannelBO {
+
+    private String locationId;
+
+    private String channelId;
+
+    private boolean enabled;
+
+}
