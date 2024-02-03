db574a9c52a32b79e1952eb143e5941b6c8dda6a
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-02 23:40:25 -0300

    Implementação do LocationChannelMapper

---
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/mappers/LocationChannelMapper.java
@@ -0,0 +1,26 @@
+package pfeffer.oms.inventory.domain.mappers;
+
+import pfeffer.oms.inventory.domain.dtos.LocationChannelDTO;
+import pfeffer.oms.inventory.domain.entities.LocationChannelBO;
+
+public class LocationChannelMapper {
+
+    public static LocationChannelDTO toDTO(LocationChannelBO bo) {
+        LocationChannelDTO dto = new LocationChannelDTO();
+
+        dto.setLocationId(bo.getLocationId());
+        dto.setChannelId(bo.getChannelId());
+
+        return dto;
+    }
+
+    public static LocationChannelBO toBO(LocationChannelDTO dto) {
+        LocationChannelBO bo = new LocationChannelBO();
+
+        bo.setLocationId(dto.getLocationId());
+        bo.setChannelId(dto.getChannelId());
+
+        return bo;
+    }
+
+}
