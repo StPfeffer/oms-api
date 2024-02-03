cb01ac7aeea7f82f602ce7a98a008351cd447b46
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-02 23:37:52 -0300

    Criação do LocationChannelDTO

---
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/LocationChannelDTO.java
@@ -0,0 +1,11 @@
+package pfeffer.oms.inventory.domain.dtos;
+
+public class LocationChannelDTO {
+
+    private String locationId;
+
+    private String channelId;
+
+    private boolean enabled;
+
+}
