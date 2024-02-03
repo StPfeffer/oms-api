9414d6321d4e322fa86f82542c5a3e27d568a17e
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-03 00:15:11 -0300

    Criação da LocationChannelController e implementação de rota para criar um LocationChannel

---
+++ inventory/src/main/java/pfeffer/oms/inventory/controllers/LocationChannelController.java
@@ -0,0 +1,28 @@
+package pfeffer.oms.inventory.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pfeffer.oms.inventory.domain.dtos.LocationChannelDTO;
+import pfeffer.oms.inventory.services.LocationChannelService;
+
+@RestController
+@RequestMapping("locations/channels")
+public class LocationChannelController {
+
+    private final LocationChannelService service;
+
+    public LocationChannelController(LocationChannelService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<LocationChannelDTO> createLocationChannel(LocationChannelDTO locationChannel) {
+        LocationChannelDTO newLocationChannel = this.service.createChannel(locationChannel);
+
+        return new ResponseEntity<>(newLocationChannel, HttpStatus.CREATED);
+    }
+
+}
