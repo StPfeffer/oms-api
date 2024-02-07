32b14649e96db2b310b7464484cc74e6050e505d
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-06 22:58:07 -0300

    Adição de propriedade isDefault ao ChannelBO

--- inventory/src/main/java/pfeffer/oms/inventory/domain/entities/ChannelBO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/entities/ChannelBO.java
@@ -3,12 +3,22 @@ package pfeffer.oms.inventory.domain.entities;
 public class ChannelBO {
     private String channelId;
 
+    private boolean isDefault;
+
     public String getChannelId() {
         return channelId;
     }
 
     public void setChannelId(String channelId) {
         this.channelId = channelId;
+    }
+
+    public boolean isDefault() {
+        return isDefault;
+    }
+
+    public void setDefault(boolean aDefault) {
+        isDefault = aDefault;
     }
 
 }
