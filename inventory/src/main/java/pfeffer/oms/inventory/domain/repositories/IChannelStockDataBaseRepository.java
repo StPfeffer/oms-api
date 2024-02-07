d3834b123942964ff867262f577b80e7dcf37072
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-06 23:03:32 -0300

    Criação da interface IChannelStockDataBaseRepository

---
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/repositories/IChannelStockDataBaseRepository.java
@@ -0,0 +1,6 @@
+package pfeffer.oms.inventory.domain.repositories;
+
+import pfeffer.oms.inventory.domain.entities.ChannelStockBO;
+
+public interface IChannelStockDataBaseRepository extends IAbstractDataBaseRepository<ChannelStockBO> {
+}
