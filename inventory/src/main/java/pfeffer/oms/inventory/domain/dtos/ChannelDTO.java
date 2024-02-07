947167135f179422b491d2770573cabc2fdb5123
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-06 22:57:43 -0300

    Adição de propriedade isDefault ao ChannelDTO

--- inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/ChannelDTO.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/dtos/ChannelDTO.java
@@ -4,12 +4,22 @@ public class ChannelDTO {
 
     private String channelId;
 
+    private boolean isDefault;
+
     public String getChannelId() {
         return channelId;
     }
 
     public void setChannelId(String channelId) {
         this.channelId = channelId;
+    }
+
+    public boolean isDefault() {
+        return isDefault;
+    }
+
+    public void setDefault(boolean isDefault) {
+        this.isDefault = isDefault;
     }
 
 }
