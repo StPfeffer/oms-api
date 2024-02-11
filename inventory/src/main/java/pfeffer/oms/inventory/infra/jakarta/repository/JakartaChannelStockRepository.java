a8843b7c6a837717f7144d26969855d5f4a6da30
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-10 21:47:25 -0300

    Implementação do método update e findChannelStockTypesByChannelId na JakartaChannelStockRepository

--- inventory/src/main/java/pfeffer/oms/inventory/infra/jakarta/repository/JakartaChannelStockRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/infra/jakarta/repository/JakartaChannelStockRepository.java
@@ -1,23 +1,37 @@
 package pfeffer.oms.inventory.infra.jakarta.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
+import pfeffer.oms.inventory.domain.dtos.ChannelStockDTO;
 import pfeffer.oms.inventory.domain.entities.ChannelStockBO;
+import pfeffer.oms.inventory.domain.exceptions.ChannelException;
+import pfeffer.oms.inventory.domain.mappers.ChannelMapper;
+import pfeffer.oms.inventory.domain.mappers.ChannelStockMapper;
 import pfeffer.oms.inventory.domain.repositories.channel.IChannelStockDataBaseRepository;
+import pfeffer.oms.inventory.domain.usecases.channel.IChannelStockRepository;
+import pfeffer.oms.inventory.infra.jakarta.mappers.JakartaChannelMapper;
 import pfeffer.oms.inventory.infra.jakarta.mappers.JakartaChannelStockMapper;
+import pfeffer.oms.inventory.infra.jakarta.model.JakartaChannel;
 import pfeffer.oms.inventory.infra.jakarta.model.JakartaChannelStock;
 
+import java.util.List;
+
 @Service
-public class JakartaChannelStockRepository extends SimpleJpaRepository<JakartaChannelStock, Long> implements IChannelStockDataBaseRepository {
+public class JakartaChannelStockRepository extends SimpleJpaRepository<JakartaChannelStock, Long> implements IChannelStockDataBaseRepository, IChannelStockRepository {
 
     private final EntityManager em;
 
+    private final JakartaChannelRepository channelRepository;
+
     @Autowired
-    public JakartaChannelStockRepository(EntityManager em) {
+    public JakartaChannelStockRepository(EntityManager em, JakartaChannelRepository channelRepository) {
         super(JakartaChannelStock.class, em);
         this.em = em;
+        this.channelRepository = channelRepository;
     }
 
     @Override
@@ -28,6 +42,41 @@ public class JakartaChannelStockRepository extends SimpleJpaRepository<JakartaCh
         em.flush();
 
         return JakartaChannelStockMapper.toDomain(entity);
+    }
+
+    @Override
+    public ChannelStockBO update(String channelId, ChannelStockBO bo) {
+        List<ChannelStockDTO> stockTypes = this.findChannelStockTypesByChannelId(channelId);
+
+        if (stockTypes.isEmpty()) {
+            throw new ChannelException("There is no channel registered with the provided id", 404);
+        }
+
+        JakartaChannel channel = JakartaChannelMapper.toEntity(ChannelMapper.toBO(channelRepository.findChannelByChannelId(channelId)));
+
+        JakartaChannelStock entity = JakartaChannelStockMapper.toEntity(bo);
+        entity.setChannel(channel);
+
+        em.merge(entity);
+        em.flush();
+
+        return JakartaChannelStockMapper.toDomain(entity);
+    }
+
+    @Override
+    public List<ChannelStockDTO> findChannelStockTypesByChannelId(String channelId) {
+        TypedQuery<JakartaChannelStock> query = em.createQuery("SELECT e FROM JakartaChannelStock e WHERE e.channel.channelId = :channelId", JakartaChannelStock.class)
+                .setParameter("channelId", channelId);
+
+        try {
+            List<JakartaChannelStock> stockTypes = query.getResultList();
+
+            return stockTypes.stream().map(jakartaChannelStock ->
+                    ChannelStockMapper.toDTO(JakartaChannelStockMapper.toDomain(jakartaChannelStock))
+            ).toList();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
 }
