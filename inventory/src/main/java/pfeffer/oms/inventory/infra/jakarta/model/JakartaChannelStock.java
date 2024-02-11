7e2c0c8c082c472d8ee23f3968f56526743a3038
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-10 21:30:41 -0300

    Criação da entidade JakartaChannelStock

---
+++ inventory/src/main/java/pfeffer/oms/inventory/infra/jakarta/model/JakartaChannelStock.java
@@ -0,0 +1,32 @@
+package pfeffer.oms.inventory.infra.jakarta.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pfeffer.oms.inventory.domain.enums.EnumStockType;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "channel_stock")
+public class JakartaChannelStock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "channel_id", nullable = false)
+    private JakartaChannel channel;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "stock_type", nullable = false)
+    private List<EnumStockType> stockTypes;
+
+}
