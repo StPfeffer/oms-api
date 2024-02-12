8fddd474a01224c43dbfacfa8f45c574d990b3cc
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-11 23:34:47 -0300

    Melhoria no JakartaChannelMapper para mapear o tipo de estoque

--- inventory/src/main/java/pfeffer/oms/inventory/infra/jakarta/mappers/JakartaChannelMapper.java
+++ inventory/src/main/java/pfeffer/oms/inventory/infra/jakarta/mappers/JakartaChannelMapper.java
@@ -1,7 +1,13 @@
 package pfeffer.oms.inventory.infra.jakarta.mappers;
 
 import pfeffer.oms.inventory.domain.entities.ChannelBO;
+import pfeffer.oms.inventory.domain.entities.ChannelStockBO;
+import pfeffer.oms.inventory.domain.enums.EnumStockType;
 import pfeffer.oms.inventory.infra.jakarta.model.JakartaChannel;
+import pfeffer.oms.inventory.infra.jakarta.model.JakartaChannelStock;
+
+import java.util.Collections;
+import java.util.List;
 
 public class JakartaChannelMapper {
 
@@ -11,6 +17,23 @@ public class JakartaChannelMapper {
         entity.setChannelId(bo.getChannelId());
         entity.setDefault(bo.isDefault());
 
+        if (bo.getStockTypes() == null || bo.getStockTypes().isEmpty()) {
+            ChannelStockBO channelStockBO = new ChannelStockBO();
+            channelStockBO.setChannelId(bo.getChannelId());
+
+            List<JakartaChannelStock> physicalStockType = Collections.singletonList(JakartaChannelStockMapper.toEntity(channelStockBO));
+
+            entity.setStockTypes(physicalStockType);
+        } else {
+            ChannelStockBO channelStockBO = new ChannelStockBO();
+            channelStockBO.setChannelId(bo.getChannelId());
+            channelStockBO.setStockTypes(bo.getStockTypes());
+
+            List<JakartaChannelStock> jakartaChannelStocks = Collections.singletonList(JakartaChannelStockMapper.toEntity(channelStockBO));
+
+            entity.setStockTypes(jakartaChannelStocks);
+        }
+
         return entity;
     }
 
@@ -19,6 +42,18 @@ public class JakartaChannelMapper {
 
         bo.setChannelId(entity.getChannelId());
         bo.setDefault(entity.isDefault());
+
+        if (entity.getStockTypes() == null || entity.getStockTypes().isEmpty()) {
+            JakartaChannelStock jakartaChannelStock = new JakartaChannelStock();
+            jakartaChannelStock.setChannel(entity);
+
+            List<ChannelStockBO> physicalStockType = Collections.singletonList(JakartaChannelStockMapper.toDomain(jakartaChannelStock));
+
+            bo.setStockTypes(physicalStockType.get(0).getStockTypes());
+        } else {
+            List<EnumStockType> stockTypes = entity.getStockTypes().stream().map(JakartaChannelStock::getStockTypes).toList().get(0);
+            bo.setStockTypes(stockTypes);
+        }
 
         return bo;
     }
