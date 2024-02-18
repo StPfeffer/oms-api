f5c0e88c11d259b7922a73eb370711ed04d9f654
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-17 22:53:34 -0300

    Propriedades definindo algumas exceções comunds na ChannelException

--- inventory/src/main/java/pfeffer/oms/inventory/domain/exceptions/ChannelException.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/exceptions/ChannelException.java
@@ -2,6 +2,10 @@ package pfeffer.oms.inventory.domain.exceptions;
 
 public class ChannelException extends InventoryException {
 
+    public static ChannelException NOT_FOUND = new ChannelException("There is no channel registered with the provided id", 404);
+
+    public static ChannelException ALREADY_EXISTS = new ChannelException("There is already a channel registered with the provided id", 400);
+
     public ChannelException(String message, Integer statusCode) {
         super(message, statusCode);
     }
