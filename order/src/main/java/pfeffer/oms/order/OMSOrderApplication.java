a01478591711a32124f4a9fb8162b5b769ecb326
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-17 22:40:01 -0300

    Melhoria para buscar os beans do módulo inventory

--- order/src/main/java/pfeffer/oms/order/OMSOrderApplication.java
+++ order/src/main/java/pfeffer/oms/order/OMSOrderApplication.java
@@ -2,8 +2,10 @@ package pfeffer.oms.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"pfeffer.oms.inventory"})
 public class OMSOrderApplication {
 
 	public static void main(String[] args) {
