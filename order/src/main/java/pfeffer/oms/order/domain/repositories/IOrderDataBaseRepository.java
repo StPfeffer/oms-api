645547a6ce825916068f230b70261f70ba849c45
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-17 21:09:54 -0300

    Criação da interface IOrderDataBaseRepository

---
+++ order/src/main/java/pfeffer/oms/order/domain/repositories/IOrderDataBaseRepository.java
@@ -0,0 +1,7 @@
+package pfeffer.oms.order.domain.repositories;
+
+import pfeffer.oms.inventory.domain.repositories.IAbstractDataBaseRepository;
+import pfeffer.oms.order.domain.entities.OrderBO;
+
+public interface IOrderDataBaseRepository extends IAbstractDataBaseRepository<OrderBO> {
+}
