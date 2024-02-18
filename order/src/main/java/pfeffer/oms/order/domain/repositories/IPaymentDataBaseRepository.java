4fcd4e600d266845f70f747817a94f0aad796134
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-17 23:23:36 -0300

    Criação da interface IPaymentDataBaseRepository

---
+++ order/src/main/java/pfeffer/oms/order/domain/repositories/IPaymentDataBaseRepository.java
@@ -0,0 +1,7 @@
+package pfeffer.oms.order.domain.repositories;
+
+import pfeffer.oms.inventory.domain.repositories.IAbstractDataBaseRepository;
+import pfeffer.oms.order.domain.entities.PaymentBO;
+
+public interface IPaymentDataBaseRepository extends IAbstractDataBaseRepository<PaymentBO> {
+}
