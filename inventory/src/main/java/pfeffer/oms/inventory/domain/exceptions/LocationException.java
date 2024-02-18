7a0153bc159d4dc4ae0a7a8ffdbdc7b498e72e76
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-17 22:51:10 -0300

    Propriedades definindo algumas exceções comuns na LocationException

--- inventory/src/main/java/pfeffer/oms/inventory/domain/exceptions/LocationException.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/exceptions/LocationException.java
@@ -2,6 +2,10 @@ package pfeffer.oms.inventory.domain.exceptions;
 
 public class LocationException extends InventoryException {
 
+    public static LocationException LOCATION_NOT_FOUND = new LocationException("There is no branch registered with the provided id", 404);
+
+    public static LocationException LOCATION_ALREADY_EXISTS = new LocationException("There is already a branch registered with the provided documents", 400);
+
     public LocationException(String message, Integer statusCode) {
         super(message, statusCode);
     }
