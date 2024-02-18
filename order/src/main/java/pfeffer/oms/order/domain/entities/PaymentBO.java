5dc876fdfec53d2c9fd4585264974615f6f5e49f
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-17 23:22:30 -0300

    Criação do PaymentBO

---
+++ order/src/main/java/pfeffer/oms/order/domain/entities/PaymentBO.java
@@ -0,0 +1,98 @@
+package pfeffer.oms.order.domain.entities;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PaymentBO {
+
+    private String type;
+
+    private BigDecimal amount;
+
+    private String acquirer;
+
+    private String currency;
+
+    private int installments;
+
+    private int paymentNumber;
+
+    private String transactionNumber;
+
+    private Date transactionDate;
+
+    private Date paymentDate;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getAcquirer() {
+        return acquirer;
+    }
+
+    public void setAcquirer(String acquirer) {
+        this.acquirer = acquirer;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public int getInstallments() {
+        return installments;
+    }
+
+    public void setInstallments(int installments) {
+        this.installments = installments;
+    }
+
+    public int getPaymentNumber() {
+        return paymentNumber;
+    }
+
+    public void setPaymentNumber(int paymentNumber) {
+        this.paymentNumber = paymentNumber;
+    }
+
+    public String getTransactionNumber() {
+        return transactionNumber;
+    }
+
+    public void setTransactionNumber(String transactionNumber) {
+        this.transactionNumber = transactionNumber;
+    }
+
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+}
