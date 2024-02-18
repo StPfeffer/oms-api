9ddc0b693760fa2fbe3de6b74c21ef8cd590eb34
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-17 21:04:02 -0300

    Criado relacionamento de um OrderDTO com CustomerDTO e AddressDTO

--- order/src/main/java/pfeffer/oms/order/domain/dtos/OrderDTO.java
+++ order/src/main/java/pfeffer/oms/order/domain/dtos/OrderDTO.java
@@ -1,40 +1,32 @@
 package pfeffer.oms.order.domain.dtos;
 
+import pfeffer.oms.inventory.domain.dtos.AddressDTO;
+import pfeffer.oms.inventory.domain.dtos.CustomerDTO;
+
 import java.util.Date;
 
 public final class OrderDTO {
 
-    /**
-     * O ID do pedido para seu respectivo canal de vendas.
-     */
-    private String id;
+    private String orderId;
 
-    /**
-     * O ID do canal de vendas no qual o pedido se originou.
-     */
     private String channelId;
 
-    /**
-     * O ID da filial no qual o pedido está vinculado.
-     */
     private String locationId;
 
-    /**
-     * A data na qual o pedido foi criado no OMS.
-     */
     private Date createdAt;
 
-    /**
-     * A data referente a última atualização do pedido.
-     */
     private Date updatedAt;
 
-    public String getId() {
-        return id;
+    private CustomerDTO customer;
+
+    private AddressDTO billingAddress;
+
+    public String getOrderId() {
+        return orderId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
     }
 
     public String getChannelId() {
@@ -43,6 +35,14 @@ public final class OrderDTO {
 
     public void setChannelId(String channelId) {
         this.channelId = channelId;
+    }
+
+    public String getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(String locationId) {
+        this.locationId = locationId;
     }
 
     public Date getCreatedAt() {
@@ -59,6 +59,22 @@ public final class OrderDTO {
 
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
+    }
+
+    public CustomerDTO getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerDTO customer) {
+        this.customer = customer;
+    }
+
+    public AddressDTO getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(AddressDTO billingAddress) {
+        this.billingAddress = billingAddress;
     }
 
 }
