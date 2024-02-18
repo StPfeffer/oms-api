151d1236089f7e8a1639f45c3ecfd40c6f6f49d3
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-17 23:06:32 -0300

    Propriedades definindo algumas exceções comunds na StockException

--- inventory/src/main/java/pfeffer/oms/inventory/domain/exceptions/StockException.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/exceptions/StockException.java
@@ -2,6 +2,10 @@ package pfeffer.oms.inventory.domain.exceptions;
 
 public class StockException extends InventoryException {
 
+    public static StockException NOT_FOUND = new StockException("There is no stock registered for the provided skuId", 404);
+
+    public static StockException NOT_FOUND_LOCATION = new StockException("There is no stock registered for the provided skuId and locationId", 404);
+
     public StockException(String message, Integer statusCode) {
         super(message, statusCode);
     }
