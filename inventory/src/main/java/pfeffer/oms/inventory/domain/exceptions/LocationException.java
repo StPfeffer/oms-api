f31304a612df59128eeb7c1a5b7956c74bd53bef
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-17 22:53:13 -0300

    Code smell

--- inventory/src/main/java/pfeffer/oms/inventory/domain/exceptions/LocationException.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/exceptions/LocationException.java
@@ -2,9 +2,9 @@ package pfeffer.oms.inventory.domain.exceptions;
 
 public class LocationException extends InventoryException {
 
-    public static LocationException LOCATION_NOT_FOUND = new LocationException("There is no branch registered with the provided id", 404);
+    public static LocationException NOT_FOUND = new LocationException("There is no branch registered with the provided id", 404);
 
-    public static LocationException LOCATION_ALREADY_EXISTS = new LocationException("There is already a branch registered with the provided documents", 400);
+    public static LocationException ALREADY_EXISTS = new LocationException("There is already a branch registered with the provided id", 400);
 
     public LocationException(String message, Integer statusCode) {
         super(message, statusCode);
