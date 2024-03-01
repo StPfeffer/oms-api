33b55d4637c27eb800c47cec79ce5619f175b90f
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-02-29 22:49:42 -0300

    Criação do FulfillmentBO

---
+++ order/src/main/java/pfeffer/oms/order/domain/entities/FulfillmentBO.java
@@ -0,0 +1,78 @@
+package pfeffer.oms.order.domain.entities;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class FulfillmentBO {
+
+    private String id;
+
+    private String orderId;
+
+    private String locationId;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    private BigDecimal price;
+
+    private BigDecimal shippingPrice;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(String locationId) {
+        this.locationId = locationId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getShippingPrice() {
+        return shippingPrice;
+    }
+
+    public void setShippingPrice(BigDecimal shippingPrice) {
+        this.shippingPrice = shippingPrice;
+    }
+
+}
