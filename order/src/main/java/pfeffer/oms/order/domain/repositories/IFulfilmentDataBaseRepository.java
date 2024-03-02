5659fcd12a42303c4cdae76f427bebf6da637f2b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-01 22:26:40 -0300

    Criação da inteface IFulfilmentDataBaseRepository

---
+++ order/src/main/java/pfeffer/oms/order/domain/repositories/IFulfilmentDataBaseRepository.java
@@ -0,0 +1,7 @@
+package pfeffer.oms.order.domain.repositories;
+
+import pfeffer.oms.inventory.domain.repositories.IAbstractDataBaseRepository;
+import pfeffer.oms.order.domain.entities.FulfillmentBO;
+
+public interface IFulfilmentDataBaseRepository extends IAbstractDataBaseRepository<FulfillmentBO> {
+}
