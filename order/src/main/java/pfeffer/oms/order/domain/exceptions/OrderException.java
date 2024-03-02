7f5faff53499a4af4feb2c14a0c3f3d50744b2b3
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-01 22:51:51 -0300

    Definição de mensagens padrões para o OrderException

--- order/src/main/java/pfeffer/oms/order/domain/exceptions/OrderException.java
+++ order/src/main/java/pfeffer/oms/order/domain/exceptions/OrderException.java
@@ -2,6 +2,10 @@ package pfeffer.oms.order.domain.exceptions;
 
 public class OrderException extends RuntimeException {
 
+    public static OrderException NOT_FOUND = new OrderException("There is no order registered with the provided orderId for the specified channelId", 404);
+
+    public static OrderException ALREADY_EXISTS = new OrderException("There is already a order registered with the provided orderId for the provided channelId", 400);
+
     private String message;
 
     private Integer statusCode;
