827ef25b3ecdeee6692630e4094b2e882ce0ae78
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-02 11:48:18 -0300

    Melhoria no FulfillmentService para validar o canal de vendas e o pedido

--- order/src/main/java/pfeffer/oms/order/service/FulfillmentService.java
+++ order/src/main/java/pfeffer/oms/order/service/FulfillmentService.java
@@ -4,9 +4,11 @@ import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import pfeffer.oms.order.domain.dtos.FulfillmentDTO;
 import pfeffer.oms.order.domain.exceptions.FulfillmentException;
+import pfeffer.oms.order.domain.exceptions.OrderException;
 import pfeffer.oms.order.domain.repositories.IFulfillmentRepository;
 import pfeffer.oms.order.domain.usecases.CreateFulfillment;
 import pfeffer.oms.order.infra.jakarta.repository.JakartaFulfillmentRepository;
+import pfeffer.oms.order.infra.jakarta.repository.JakartaOrderRepository;
 
 import java.util.List;
 
@@ -15,12 +17,23 @@ public class FulfillmentService implements IFulfillmentRepository {
 
     private final JakartaFulfillmentRepository repository;
 
-    public FulfillmentService(JakartaFulfillmentRepository repository) {
+    private final JakartaOrderRepository orderRepository;
+
+    public FulfillmentService(JakartaFulfillmentRepository repository, JakartaOrderRepository orderRepository) {
         this.repository = repository;
+        this.orderRepository = orderRepository;
     }
 
     @Transactional
-    public FulfillmentDTO createFulfillment(FulfillmentDTO dto) {
+    public FulfillmentDTO createFulfillment(String channelId, String orderId, FulfillmentDTO dto) {
+        if (!channelId.equals(dto.getChannelId())) {
+            throw FulfillmentException.DIFF_CHANNEL;
+        }
+
+        if (orderRepository.findJakartaOrderByOrderIdAndChannelId(orderId, channelId) == null) {
+            throw OrderException.NOT_FOUND;
+        }
+
         CreateFulfillment createFulfillment = new CreateFulfillment(repository);
 
         return createFulfillment.execute(dto);
