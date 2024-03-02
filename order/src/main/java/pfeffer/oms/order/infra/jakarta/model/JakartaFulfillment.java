f6cf23271725c2e940daedf466b163f5e3791141
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-01 22:30:22 -0300

    Criação da entidade JakartaFulfillment

---
+++ order/src/main/java/pfeffer/oms/order/infra/jakarta/model/JakartaFulfillment.java
@@ -0,0 +1,50 @@
+package pfeffer.oms.order.infra.jakarta.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pfeffer.oms.inventory.infra.jakarta.model.JakartaLocation;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "fulfillment")
+public class JakartaFulfillment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "fulfillment_id", nullable = false)
+    private String fulfillmentId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "shipping_price", nullable = false)
+    private BigDecimal shippingPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private JakartaOrder order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id", nullable = false)
+    private JakartaLocation location;
+
+}
