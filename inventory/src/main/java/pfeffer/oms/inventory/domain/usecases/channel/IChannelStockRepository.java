930d497eb57476faaf5030f9742d8344d3353af4
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-02 11:38:12 -0300

    Alteração na nomenclatura de contrato na IChannelStockRepository

--- inventory/src/main/java/pfeffer/oms/inventory/domain/usecases/channel/IChannelStockRepository.java
+++ inventory/src/main/java/pfeffer/oms/inventory/domain/usecases/channel/IChannelStockRepository.java
@@ -2,10 +2,8 @@ package pfeffer.oms.inventory.domain.usecases.channel;
 
 import pfeffer.oms.inventory.domain.dtos.ChannelStockDTO;
 
-import java.util.List;
-
 public interface IChannelStockRepository {
 
-    ChannelStockDTO findChannelStockByChannelId(String channelId);
+    ChannelStockDTO findByChannelId(String channelId);
 
 }
