fa5f9c45f5e4e77c0e4890f4cc42abea7234170c
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-02 11:48:01 -0300

    Ajuste nas mensagens das exceções do FulfillmentException

--- order/src/main/java/pfeffer/oms/order/domain/exceptions/FulfillmentException.java
+++ order/src/main/java/pfeffer/oms/order/domain/exceptions/FulfillmentException.java
@@ -2,6 +2,10 @@ package pfeffer.oms.order.domain.exceptions;
 
 public class FulfillmentException extends OrderException {
 
+    public static FulfillmentException NOT_FOUND = new FulfillmentException("There is no fulfillment registered with the provided orderId and fulfillmentId for the specified channelId", 404);
+
+    public static FulfillmentException ALREADY_EXISTS = new FulfillmentException("There is already a fulfillment registered with the provided orderId, channelId and fulfillmentId", 400);
+
     public FulfillmentException(String message, Integer statusCode) {
         super(message, statusCode);
     }
