d576693bbca5e6f4ca06e80759a0df5f08d66291
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-02 11:23:25 -0300

    Implementação de endpoint para buscar um pedido
     Ajuste para informar o ID do canal de vendas no path

--- order/src/main/java/pfeffer/oms/order/controller/OrderController.java
+++ order/src/main/java/pfeffer/oms/order/controller/OrderController.java
@@ -3,15 +3,12 @@ package pfeffer.oms.order.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pfeffer.oms.order.domain.dtos.OrderDTO;
 import pfeffer.oms.order.service.OrderService;
 
 @RestController
-@RequestMapping("orders")
+@RequestMapping("channels/{channelId}/orders")
 public class OrderController {
 
     private final OrderService service;
@@ -22,10 +19,17 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO order) {
-        OrderDTO newOrder = this.service.createOrder(order);
+    public ResponseEntity<OrderDTO> createOrder(@PathVariable String channelId, @RequestBody OrderDTO order) {
+        OrderDTO newOrder = this.service.createOrder(channelId, order);
 
         return new ResponseEntity<>(newOrder, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{orderId}")
+    public ResponseEntity<OrderDTO> getOrder(@PathVariable String channelId, @PathVariable String orderId) {
+        OrderDTO order = this.service.findOrderByOrderIdAndChannelId(orderId, channelId);
+
+        return new ResponseEntity<>(order, HttpStatus.OK);
     }
 
 }
