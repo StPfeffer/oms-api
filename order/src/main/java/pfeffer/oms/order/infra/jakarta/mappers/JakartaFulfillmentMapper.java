0b4be0b3cad6501616af0bbe5c36c1c40c691bce
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-01 22:32:21 -0300

    Implementação do JakartaFulfillmentMapper

---
+++ order/src/main/java/pfeffer/oms/order/infra/jakarta/mappers/JakartaFulfillmentMapper.java
@@ -0,0 +1,34 @@
+package pfeffer.oms.order.infra.jakarta.mappers;
+
+import pfeffer.oms.order.domain.entities.FulfillmentBO;
+import pfeffer.oms.order.infra.jakarta.model.JakartaFulfillment;
+
+public class JakartaFulfillmentMapper {
+
+    public static JakartaFulfillment toEntity(FulfillmentBO domain) {
+        JakartaFulfillment entity = new JakartaFulfillment();
+
+        entity.setFulfillmentId(domain.getFulfillmentId());
+        entity.setCreatedAt(domain.getCreatedAt());
+        entity.setUpdatedAt(domain.getUpdatedAt());
+        entity.setPrice(domain.getPrice());
+        entity.setShippingPrice(domain.getShippingPrice());
+
+        return entity;
+    }
+
+    public static FulfillmentBO toDomain(JakartaFulfillment entity) {
+        FulfillmentBO domain = new FulfillmentBO();
+
+        domain.setFulfillmentId(entity.getFulfillmentId());
+        domain.setCreatedAt(entity.getCreatedAt());
+        domain.setUpdatedAt(entity.getUpdatedAt());
+        domain.setPrice(entity.getPrice());
+        domain.setShippingPrice(entity.getShippingPrice());
+        domain.setLocationId(entity.getLocation().getLocationId());
+        domain.setOrderId(entity.getOrder().getOrderId());
+
+        return domain;
+    }
+
+}
