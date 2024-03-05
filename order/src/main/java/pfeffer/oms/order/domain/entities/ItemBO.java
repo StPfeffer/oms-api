f89384070137d78f79a22fb03ab8e90f471b5411
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-04 22:40:23 -0300

    Criação do ItemBO

---
+++ order/src/main/java/pfeffer/oms/order/domain/entities/ItemBO.java
@@ -0,0 +1,47 @@
+package pfeffer.oms.order.domain.entities;
+
+import java.math.BigDecimal;
+
+public class ItemBO {
+
+    private String skuId;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+
+    private BigDecimal shippingPrice;
+
+    public String getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(String skuId) {
+        this.skuId = skuId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getShippingPrice() {
+        return shippingPrice;
+    }
+
+    public void setShippingPrice(BigDecimal shippingPrice) {
+        this.shippingPrice = shippingPrice;
+    }
+
+}
