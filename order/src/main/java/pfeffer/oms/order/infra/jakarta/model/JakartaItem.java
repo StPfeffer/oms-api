a575eb2fb5360b46f3bb442b7fe9f49588a76fd0
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-05 20:43:34 -0300

    Criação da entidade JakartaItem

---
+++ order/src/main/java/pfeffer/oms/order/infra/jakarta/model/JakartaItem.java
@@ -0,0 +1,47 @@
+package pfeffer.oms.order.infra.jakarta.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "item")
+public class JakartaItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sku_id", nullable = false)
+    private String skuId;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "width", nullable = false)
+    private Integer width;
+
+    @Column(name = "length", nullable = false)
+    private Integer length;
+
+    @Column(name = "height", nullable = false)
+    private Integer height;
+
+    @Column(name = "weight", nullable = false)
+    private Integer weight;
+
+    @Column(name = "price", precision = 10, scale = 4, nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "shipping_price", precision = 10, scale = 4, nullable = false)
+    private BigDecimal shippingPrice;
+
+}
