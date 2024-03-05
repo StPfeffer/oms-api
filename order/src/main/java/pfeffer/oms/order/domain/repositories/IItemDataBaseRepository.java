f830b787acbbd3c3170979a7ec4a3688a660d337
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-05 20:37:36 -0300

    Criação da interface IItemDataBaseRepository

---
+++ order/src/main/java/pfeffer/oms/order/domain/repositories/IItemDataBaseRepository.java
@@ -0,0 +1,7 @@
+package pfeffer.oms.order.domain.repositories;
+
+import pfeffer.oms.inventory.domain.repositories.IAbstractDataBaseRepository;
+import pfeffer.oms.order.domain.entities.ItemBO;
+
+public interface IItemDataBaseRepository extends IAbstractDataBaseRepository<ItemBO> {
+}
