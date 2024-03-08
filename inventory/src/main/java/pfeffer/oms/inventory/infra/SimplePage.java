8f56031e5001864e746aa1ebd5c9a87880fec1a1
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-07 23:06:27 -0300

    Ajuste no SimplePage

--- inventory/src/main/java/pfeffer/oms/inventory/infra/SimplePage.java
+++ inventory/src/main/java/pfeffer/oms/inventory/infra/SimplePage.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -16,6 +17,10 @@ public class SimplePage<T> extends PageImpl<T> {
                       @JsonProperty("page") final int page, @JsonProperty("size") final int size) {
 
         super(data, PageRequest.of(page, size), totalElements);
+    }
+
+    public SimplePage(final List<T> data, final Pageable pageable, final long totalElements) {
+        super(data, pageable, totalElements);
     }
 
 }
