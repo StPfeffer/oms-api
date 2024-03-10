4a096a28d26b6c263b7fe0dd867dd313a25e7704
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-09 22:14:16 -0300

    Criação do CarrierDTO

---
+++ logistic/src/main/java/pfeffer/oms/logistic/domain/dtos/CarrierDTO.java
@@ -0,0 +1,25 @@
+package pfeffer.oms.logistic.domain.dtos;
+
+public class CarrierDTO {
+
+    private String carrierId;
+
+    private String name;
+
+    public String getCarrierId() {
+        return carrierId;
+    }
+
+    public void setCarrierId(String carrierId) {
+        this.carrierId = carrierId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
