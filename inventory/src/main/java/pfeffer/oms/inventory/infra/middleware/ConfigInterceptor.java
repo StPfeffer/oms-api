5f05ca9aa216ab6db5dfac29fbd3246cc75278c9
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-09 22:14:43 -0300

    Adicionada camada de middleware ao inventory

---
+++ inventory/src/main/java/pfeffer/oms/inventory/infra/middleware/ConfigInterceptor.java
@@ -0,0 +1,19 @@
+package pfeffer.oms.inventory.infra.middleware;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Component
+public class ConfigInterceptor implements WebMvcConfigurer {
+
+    private final Interceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor);
+    }
+
+}
