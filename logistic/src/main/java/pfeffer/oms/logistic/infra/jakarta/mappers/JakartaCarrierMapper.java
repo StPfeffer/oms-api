6a01d3094246793d428fd20e464df203aa3507fb
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-09 22:21:51 -0300

    Implementação do JakartaCarrierMapper

---
+++ logistic/src/main/java/pfeffer/oms/logistic/infra/jakarta/mappers/JakartaCarrierMapper.java
@@ -0,0 +1,30 @@
+package pfeffer.oms.logistic.infra.jakarta.mappers;
+
+import pfeffer.oms.logistic.domain.entities.CarrierBO;
+import pfeffer.oms.logistic.infra.jakarta.model.JakartaCarrier;
+
+public class JakartaCarrierMapper {
+
+    public static CarrierBO toDomain(JakartaCarrier entity) {
+        CarrierBO domain = new CarrierBO();
+
+        domain.setCarrierId(entity.getCarrierId());
+        domain.setName(entity.getName());
+        domain.setCreatedAt(entity.getCreatedAt());
+        domain.setUpdatedAt(entity.getUpdatedAt());
+
+        return domain;
+    }
+
+    public static JakartaCarrier toDomain(CarrierBO domain) {
+        JakartaCarrier entity = new JakartaCarrier();
+
+        entity.setCarrierId(domain.getCarrierId());
+        entity.setName(domain.getName());
+        entity.setCreatedAt(domain.getCreatedAt());
+        entity.setUpdatedAt(domain.getUpdatedAt());
+
+        return entity;
+    }
+
+}
