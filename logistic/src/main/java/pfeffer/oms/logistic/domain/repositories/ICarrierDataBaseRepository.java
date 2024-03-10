c59ab9d162cef5cc3ea65b81ec3424624b249726
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-09 22:16:25 -0300

    Criação da interface ICarrierDataBaseRepository

---
+++ logistic/src/main/java/pfeffer/oms/logistic/domain/repositories/ICarrierDataBaseRepository.java
@@ -0,0 +1,7 @@
+package pfeffer.oms.logistic.domain.repositories;
+
+import pfeffer.oms.inventory.domain.repositories.IAbstractDataBaseRepository;
+import pfeffer.oms.logistic.domain.entities.CarrierBO;
+
+public interface ICarrierDataBaseRepository extends IAbstractDataBaseRepository<CarrierBO> {
+}
