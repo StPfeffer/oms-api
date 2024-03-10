1448463403e159cf2db9ad38d76aa096959e81bd
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-09 22:14:22 -0300

    Criação do CarrierBO

---
+++ logistic/src/main/java/pfeffer/oms/logistic/domain/entities/CarrierBO.java
@@ -0,0 +1,25 @@
+package pfeffer.oms.logistic.domain.entities;
+
+public class CarrierBO {
+
+    private String carrierId;
+
+    private String name;
+
+    public String getCarrierId() {
+        return carrierId;
+    }
+
+    public void setCarrierId(String carrierId) {
+        this.carrierId = carrierId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
