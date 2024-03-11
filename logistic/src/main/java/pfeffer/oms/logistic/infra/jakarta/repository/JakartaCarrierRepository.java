a657c1963045415f269de5f8d8412ccb7b5678e0
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-11 19:22:24 -0300

    Implementação dos métodos na JakartaCarrierRepository

--- logistic/src/main/java/pfeffer/oms/logistic/infra/jakarta/repository/JakartaCarrierRepository.java
+++ logistic/src/main/java/pfeffer/oms/logistic/infra/jakarta/repository/JakartaCarrierRepository.java
@@ -1,15 +1,22 @@
 package pfeffer.oms.logistic.infra.jakarta.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
+import pfeffer.oms.logistic.domain.dtos.CarrierDTO;
 import pfeffer.oms.logistic.domain.entities.CarrierBO;
+import pfeffer.oms.logistic.domain.exceptions.CarrierException;
+import pfeffer.oms.logistic.domain.mappers.CarrierMapper;
 import pfeffer.oms.logistic.domain.repositories.ICarrierDataBaseRepository;
+import pfeffer.oms.logistic.domain.repositories.ICarrierRepository;
+import pfeffer.oms.logistic.infra.jakarta.mappers.JakartaCarrierMapper;
 import pfeffer.oms.logistic.infra.jakarta.model.JakartaCarrier;
 
 @Service
-public class JakartaCarrierRepository extends SimpleJpaRepository<JakartaCarrier, Long> implements ICarrierDataBaseRepository {
+public class JakartaCarrierRepository extends SimpleJpaRepository<JakartaCarrier, Long> implements ICarrierDataBaseRepository, ICarrierRepository {
 
     private final EntityManager em;
 
@@ -20,8 +27,35 @@ public class JakartaCarrierRepository extends SimpleJpaRepository<JakartaCarrier
     }
 
     @Override
-    public CarrierBO persist(CarrierBO carrierBO) {
-        return null;
+    public CarrierBO persist(CarrierBO bo) {
+        JakartaCarrier entity = JakartaCarrierMapper.toEntity(bo);
+
+        this.canCreate(entity);
+
+        em.persist(entity);
+        em.flush();
+
+        return JakartaCarrierMapper.toDomain(entity);
+    }
+
+    private void canCreate(JakartaCarrier entity) {
+        CarrierDTO carrier = this.findByCarrierId(entity.getCarrierId());
+
+        if (carrier != null) {
+            throw CarrierException.ALREADY_EXISTS;
+        }
+    }
+
+    @Override
+    public CarrierDTO findByCarrierId(String carrierId) {
+        TypedQuery<JakartaCarrier> query = em.createQuery("SELECT e FROM JakartaCarrier e WHERE e.carrierId = :carrierId", JakartaCarrier.class)
+                .setParameter("carrierId", carrierId);
+
+        try {
+            return CarrierMapper.toDTO(JakartaCarrierMapper.toDomain(query.getSingleResult()));
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
 }
