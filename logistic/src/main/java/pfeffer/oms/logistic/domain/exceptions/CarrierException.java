274fabd4479aed891d281d364ffc670f624c5e0c
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-12 23:02:14 -0300

    Definindo uma exceção 404 no CarrierException

--- logistic/src/main/java/pfeffer/oms/logistic/domain/exceptions/CarrierException.java
+++ logistic/src/main/java/pfeffer/oms/logistic/domain/exceptions/CarrierException.java
@@ -4,6 +4,8 @@ public class CarrierException extends LogisticException {
 
     public static CarrierException ALREADY_EXISTS = new CarrierException("There is already a carrier registered with the provided carrierId", 400);
 
+    public static CarrierException NOT_FOUND = new CarrierException("There is no carrier registered with the provided carrierId", 404);
+
     public CarrierException(String message, Integer statusCode) {
         super(message, statusCode);
     }
