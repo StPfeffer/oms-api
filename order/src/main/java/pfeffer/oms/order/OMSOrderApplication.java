fa45ca3ea9adf12d55cfbf23d0b1ec8572fafda2
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-12 23:04:45 -0300

    Ajuste no scan da aplicação do Order para considerar o Logistic

--- order/src/main/java/pfeffer/oms/order/OMSOrderApplication.java
+++ order/src/main/java/pfeffer/oms/order/OMSOrderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"pfeffer.oms.inventory"})
+@ComponentScan(basePackages = {"pfeffer.oms.inventory", "pfeffer.oms.logistic"})
 public class OMSOrderApplication {
 
 	public static void main(String[] args) {
